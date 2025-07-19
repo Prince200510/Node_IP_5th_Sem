423fcf56f78fbc047283343d6a58200e96140b7b
Prince200510 <princemaurya8879@gmail.com>
2025-07-19 15:33:33 +0530

    Day 03: Final Update

--- express/controllers/index.ts
+++ express/controllers/index.ts
@@ -1,19 +1,53 @@
+
 import express, { Request, Response, NextFunction } from 'express';
 import { middleWareWrapper } from '../tools';
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const router = express.Router();
 
 
 router.all(
-    '/',
+    '/1',
     middleWareWrapper(async (request: Request, response: Response, next: NextFunction) => {
-        request.body = { text: 'Hello World!!!' };
+        const filePath = path.join(__dirname, '../data/student.json');
+        let students = [];
+        try {
+            const fileContent = fs.readFileSync(filePath, 'utf8');
+            students = JSON.parse(fileContent); 
+        } catch (err) {
+            students = [];
+        }
+        // request.body = { students };
+        // request.body = students.filter((student: any) => student.name === 'Prince Maurya');
+        // request.body = students.slice(0,2);
+
+        const readfilepath = path.join(__dirname, '../data/read.json');
+        let readdata = { reads: 0 };
+        try {
+            if (fs.existsSync(readfilepath)) {
+                const readfile = fs.readFileSync(readfilepath, 'utf8');
+                readdata = JSON.parse(readfile);
+            }
+        } catch (err) {
+            readdata = { reads: 0 };
+        }
+        readdata.reads += 1;
+        fs.writeFileSync(readfilepath, JSON.stringify(readdata, null, 2));
+
+        request.body = { students: students.filter((student: any) => student.name === 'Prince Maurya') };
         next();
     }),
+
     middleWareWrapper(async (request: Request, response: Response) => {
         response.render('hello', {
             title: 'Lets Go',
-            message: request.body.text
+            students: request.body.students
         });
     })
 );
